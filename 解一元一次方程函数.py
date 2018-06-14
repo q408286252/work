a22f834d63852516f54b311fb948b240db27c114
q408286252 <32360993+q408286252@users.noreply.github.com>
2018-06-14 21:21:00 +0800

    Delete 解一元一次方程函数.py

--- 解一元一次方程函数.py
+++
@@ -1,5 +0,0 @@
-#只能解一元一次方程  它把变量换算成虚数然后
-def solve(eq,variable='x'): #variable 变量
-  eq1 = eq.replace("=","-(") + ")" #吧'=' 换成'-(' 字符尾部加')'
-  c = eval(eq1,{variable:1j})  #变量变为1单位虚数
-  return -c.real/c.imag  #-实数值/虚数值 = x值
