e121e99715814f4b1c67e056cdf0d2481b1e80d6
q408286252 <32360993+q408286252@users.noreply.github.com>
2018-06-14 21:17:33 +0800

    Create 算法.py

---
+++ 算法.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#冒泡排序
+def maopaopaixu(list1):
+    for a in range(len(list1)-1):
+        for i in range(len(list1)-1):
+            if list1[i] > list1[i+1]:
+                list1[i] ,list1[i+1] = list1[i+1],list1[i]
+    return list1
+list_0 = [9,8,7,6,5,4,3,2,1,0]
+maopaopaixu(list_0)
