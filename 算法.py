0f18a9e1439f125d12a168341cc537aac0ffacc6
q408286252 <32360993+q408286252@users.noreply.github.com>
2018-06-14 21:23:11 +0800

    Update 算法.py

--- 算法.py
+++ 算法.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#只能解一元一次方程 实质是它把未知数换成虚数然后求解
-def solve(eq,variable='x'): #variable 变量
-  eq1 = eq.replace("=","-(") + ")" #吧'=' 换成'-(' 字符尾部加')'
-  c = eval(eq1,{variable:1j})  #变量变为1单位虚数
-  return -c.real/c.imag  #-实数值/虚数值 = x值
-
 #冒泡排序
 def maopaopaixu(list1):
     for a in range(len(list1)-1):
